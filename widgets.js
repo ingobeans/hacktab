54628b826cca777783cf546e8c32986bd699f828
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-09-17 23:56:11 +0200

    Allow multiline for text widget

--- widgets.js
+++ widgets.js
@@ -6,7 +6,7 @@ let addWidgetSelect = document.getElementById("add-widget-select");
 
 function format(str, values, undefinedLookup = (key) => { return `<undefined ${key}>` }) {
     return str.replace(/{([a-zA-Z]+)}/g, function (match, index) {
-        return typeof values[index] !== 'undefined' ? values[index] : undefinedLookup(index);
+        return (typeof values[index] !== 'undefined' ? values[index] : undefinedLookup(index)).replaceAll("\n", "<br>");
     });
 }
 
@@ -15,7 +15,7 @@ function capitalizeFirst(text = "") {
 }
 
 function formatWidget(str, values, widgetKey) {
-    return format(str, values, findDefaultSettingValue.bind(null, widgetKey))
+    return format(str, values, findDefaultSettingValue.bind(null, widgetKey));
 }
 
 // Source - https://stackoverflow.com/a/5306832
